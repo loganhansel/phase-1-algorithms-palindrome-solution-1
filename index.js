9ddbcad00f520e924582a53875a9c0dfa66e34e4
loganhansel <loganhansel23@gmail.com>
2022-07-15 22:27:24 -0400

    commit tree 87c44ccaecce672bb14488b6688470adff255a4a

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word)
+  return word === reversedWord
+}
+
+function reverse(word) {
+  return word.split("").reverse().join("")
 }
 
 /* 
